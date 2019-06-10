42083429fee3b6c3fecd00566888ad59f0c4ef06
yj0930 <yj0930@gmail.com>
2019-06-10 15:36:38 +0800

    new xUnitTest

---
+++ FakeCredentials/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FakeCredentials")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FakeCredentials")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("042de5e2-4153-47e2-b0f4-312166594cfa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FakeCredentials/xUnitFake.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using OWASP.WebGoat.NET.App_Code;
+using OWASP.WebGoat.NET.App_Code.DB;
+using Xunit;
+
+namespace FakeCredentials
+{
+    public class xUnitFake
+    {
+        [Fact]
+        public void FakeTest2()
+        {
+            // Arrange
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string configName = Settings.DefaultConfigName;
+            configName = Path.Combine(baseDirectory, configName);
+            string sqliteFile = "webgoat_coins.sqlite";
+            sqliteFile = Path.Combine(baseDirectory, sqliteFile);
+            string[] lines =
+            {
+                "dbtype=Sqlite",
+                "filename=" + sqliteFile
+            };
+            File.WriteAllLines(configName, lines);
+            ConfigFile configFile = new ConfigFile(configName);
+            configFile.Load();
+            IDbProvider dbProvider = DbProviderFactory.Create(configFile);
+            string fakeEmail = "someone@somewhere";
+            string fakePassword = DateTime.Now.ToString();
+            string goodEmail = "bob@ateliergraphique.com";
+            string goodPassword = Encoder.Decode("MTIzNDU2");
+            string hackEmail = "' or 1 = 1 --";
+            string hackPassword = "";
+
+            // Act
+            bool loginOk = dbProvider.IsValidCustomerLogin(fakeEmail, fakePassword);
+            bool loginFail = dbProvider.IsValidCustomerLogin(goodEmail, goodPassword);
+            bool hackFail = dbProvider.IsValidCustomerLogin(hackEmail, hackPassword);
+
+            // Assert
+            Assert.True(loginOk);
+            Assert.False(loginFail);
+            Assert.False(hackFail);
+        }
+    }
+}
